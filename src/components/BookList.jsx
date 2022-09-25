14534e88f01d276585e347d2a7874ec87cce3733
Alvin Yanson <yansonalvin29@gmail.com>
2022-09-25 17:01:18 +0800

    done

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,16 +1,21 @@
 import React, { useContext } from 'react';
+import { BookContext } from '../contexts/BookContext';
 import { ThemeContext } from '../contexts/ThemeContext';
 
 const BookList = () => {
-    const { isLightTheme, light, dark } = useContext(ThemeContext)
+    const { isLightTheme, light, dark } = useContext(ThemeContext);
+    const { books } = useContext(BookContext);
     const theme = isLightTheme ? light : dark;
 
     return (
         <div className='book-list' style={{ background: theme.bg, color: theme.syntax }}>
             <ul>
-                <li style={{ background: theme.ui }}>the way of kings</li>
-                <li style={{ background: theme.ui }}>the name of the wind</li>
-                <li style={{ background: theme.ui }}>the final empire</li>
+                {books.map((book) => {
+                    return (
+                        <li key={book.id} style={{ background: theme.ui }}>{book.title}
+                        </li>
+                    )
+                })}
             </ul>
         </div>
     )
