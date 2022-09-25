465a7e337fef2e5d96e681ea8fa17e57f42b27d2
Alvin Yanson <yansonalvin29@gmail.com>
2022-09-25 15:46:20 +0800

    done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import React from 'react';
-import BookList from './components/BookList';
-import Navbar from './components/Navbar';
-import ThemeContextProvider from './contexts/ThemeContext';
-import ThemeToggle from './components/ThemeToggle';
-import AuthContextProvider from './contexts/AuthContext';
+import Songlist from './components/Songlist';
 
 function App() {
   return (
     <div className="App">
-      <ThemeContextProvider>
-        <AuthContextProvider>
-          <Navbar />
-          <BookList />
-          <ThemeToggle />
-        </AuthContextProvider>
-      </ThemeContextProvider>
+      <Songlist />
     </div>
   );
 }
---
+++ src/components/Songlist.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Songlist = () => {
+    return (
+        <div className='song-list'>
+            <ul>
+                <li>this wild darkness</li>
+                <li>memory gospel</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Songlist
