8c3372d9e3681585199042bf01fc2d910df7db6c
Alvin Yanson <yansonalvin29@gmail.com>
2022-09-25 16:42:44 +0800

    done

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,17 +1,13 @@
-import React, { Component } from 'react'
+import React, { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
+const ThemeToggle = () => {
+    const { toggleTheme } = useContext(ThemeContext)
 
-class ThemeToggle extends Component {
-    static contextType = ThemeContext;
-
-    render() {
-        const { toggleTheme } = this.context;
-
-        return (
-            <button onClick={toggleTheme}>Toggle Theme</button>
-        );
-    }
+    return (
+        <button onClick={toggleTheme}>Toggle Theme</button>
+    )
 }
+
 
 export default ThemeToggle;
